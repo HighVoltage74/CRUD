a26c6f62aa5e5f6afe2c7388590dfebb1d9b66de
John Kuchta <John.Kuchta@gmail.com>
2016-12-03 16:39:48 -0600

    CRUD first commit

---
+++ src/crud/Crud.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package crud;
+
+/**
+ *
+ * @author kuchtaj
+ */
+public class Crud {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //start login form
+        Login loginForm = new Login();
+        loginForm.setVisible(true);
+        loginForm.setTitle("Login");
+        loginForm.setLocationRelativeTo(null);
+        
+        
+        //InterfaceCRUD test = new InterfaceCRUD();
+        
+        
+    }
+    
+}
